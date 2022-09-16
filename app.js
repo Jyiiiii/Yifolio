16bd99b0dd06476bd97a97c9a312de782e2e7ee8
Jyiiiii <jyiiii047@gmail.com>
2022-09-16 22:06:12 +0200

    Add portfolio page. Finished#2

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const expressHandlebars = require("express-handlebars");
+const data = require("./data.js");
 
 const app = express();
 
@@ -14,6 +15,13 @@ app.use(express.static("public"));
 
 app.get("/", function (request, response) {
   response.render("start.hbs");
+});
+
+app.get("/portfolio", function (request, response) {
+  const model = {
+    portfolioWorks: data.portfolioWorks,
+  };
+  response.render("portfolio.hbs", model);
 });
 
 app.listen(8080);
---
+++ data.js
@@ -0,0 +1,17 @@
+exports.portfolioWorks = [
+  {
+    id: 1,
+    image: image,
+    title: "Band Tour Poster",
+  },
+  {
+    id: 2,
+    image: image,
+    title: "Magazine",
+  },
+  {
+    id: 3,
+    image: image,
+    title: "Yijia Public Service",
+  },
+];
