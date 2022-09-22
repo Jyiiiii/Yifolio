a02e115c8bf775fed05e713813e48dfb19f7ee56
Jyiiiii <jyiiii047@gmail.com>
2022-09-22 21:51:27 +0200

    finish part of #6

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const expressHandlebars = require("express-handlebars");
-const data = require("./data.js");
+// const data = require("./data.js");
+const bodyParser = require("body-parser");
+const sqlite3 = require("sqlite3");
+
+const database = new sqlite3.Database("Yifolio-database.db");
 
 const app = express();
 
@@ -13,6 +17,12 @@ app.engine(
 
 app.use(express.static("public"));
 
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+
 app.get("/", function (request, response) {
   response.render("start.hbs");
 });
@@ -21,7 +31,38 @@ app.get("/projects", function (request, response) {
   const model = {
     projects: data.projects,
   };
+
   response.render("projects.hbs", model);
+});
+
+app.get("/projects/add", function (request, response) {
+  response.render("create-newproject.hbs");
+});
+
+app.post("/projects/add", function (request, response) {
+  const title = request.body.title;
+
+  const intro = request.body.intro;
+
+  const project = {
+    title,
+    intro,
+    id: data.projects.length + 1,
+  };
+
+  data.projects.push(project);
+
+  response.redirect("/projects");
+});
+
+app.post("/delete-project/:id", function (request, response) {
+  const id = request.params.id;
+
+  const projectIndex = data.projects.findIndex((p) => p.id == id);
+
+  data.projects.slice(projectIndex, 1);
+
+  response.redirect("/projects");
 });
 
 app.get("/projects/:id", function (request, response) {
@@ -39,6 +80,7 @@ app.get("/blogs", function (request, response) {
   const model = {
     blogs: data.blogs,
   };
+
   response.render("blogs.hbs", model);
 });
 
@@ -50,6 +92,7 @@ app.get("/blogs/:id", function (request, response) {
   const model = {
     blog: blog,
   };
+
   response.render("blog.hbs", model);
 });
 
--- data.js
+++ data.js
@@ -2,14 +2,17 @@ exports.projects = [
   {
     id: 1,
     title: "Band Tour Poster",
+    intro: "dblbcuwfcfbrhbfr",
   },
   {
     id: 2,
     title: "Magazine",
+    intro: "dblbcuwfcfbrhbfr",
   },
   {
     id: 3,
     title: "Yijia Public Service",
+    intro: "dblbcuwfcfbrhbfr",
   },
 ];
 
